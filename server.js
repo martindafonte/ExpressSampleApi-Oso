2fc12cf8221aba9202593a2fda9275f80974b7d3
Martín Da Fonte <martindafonte@gmail.com>
2017-11-07 16:39:04 -0300

    chore: borrado de comentarios y cambio de title

--- server.js
+++ server.js
@@ -4,11 +4,10 @@
 // init project
 var express = require('express');
 var oso_server = require('./src/oso-api');
-var app = oso_server.app;
 
+var app = oso_server.app;
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
